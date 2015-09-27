a606f2770948cb42eede4b49b92e54743c7d000a
Paolo Ambrosio <git@paolo.ambrosio.name>
2015-09-27 16:06:21 +0100

    Skeleton

---
+++ build.sbt
@@ -0,0 +1,18 @@
+name         := "akka-http-authorization"
+organization := "net.paoloambrosio"
+version      := "1.0"
+scalaVersion := "2.11.7"
+
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+libraryDependencies ++= {
+  val akkaV       = "2.3.14"
+  val akkaStreamV = "1.0"
+  Seq(
+    "com.typesafe.akka" %% "akka-actor"                           % akkaV,
+    "com.typesafe.akka" %% "akka-stream-experimental"             % akkaStreamV,
+    "com.typesafe.akka" %% "akka-http-core-experimental"          % akkaStreamV,
+    "com.typesafe.akka" %% "akka-http-experimental"               % akkaStreamV
+  )
+}
+
---
+++ src/main/scala/AkkaHttpAuthorization.scala
@@ -0,0 +1,37 @@
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.{ActorMaterializer, Materializer}
+import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.concurrent.ExecutionContextExecutor
+
+
+trait Service {
+  implicit val system: ActorSystem
+  implicit def executor: ExecutionContextExecutor
+  implicit val materializer: Materializer
+
+  def config: Config
+  val logger: LoggingAdapter
+
+  val routes = {
+    logRequestResult("akka-http") {
+      get {
+        complete("Resource content\n")
+      }
+    }
+  }
+}
+
+object AkkaHttpAuthorization extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+}
