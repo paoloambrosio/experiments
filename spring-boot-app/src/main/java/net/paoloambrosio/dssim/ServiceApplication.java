4c3a89a7341daaea364404d698c62ff593a52671
Paolo Ambrosio <git@paolo.ambrosio.name>
2015-11-08 20:59:43 +0000

    Part 1 - Blocking I/O for Tomcat

--- spring-boot-app/src/main/java/net/paoloambrosio/dssim/ServiceApplication.java
+++ spring-boot-app/src/main/java/net/paoloambrosio/dssim/ServiceApplication.java
@@ -2,11 +2,15 @@ package net.paoloambrosio.dssim;
 
 import net.paoloambrosio.dssim.slowdown.SlowdownProvider;
 import net.paoloambrosio.dssim.slowdown.SlowdownProviderFactory;
+import org.apache.catalina.connector.Connector;
 import org.jmxtrans.embedded.EmbeddedJmxTrans;
 import org.jmxtrans.embedded.config.ConfigurationParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -35,5 +39,25 @@ public class ServiceApplication {
         EmbeddedJmxTrans bean = new ConfigurationParser().newEmbeddedJmxTrans("classpath:jmxtrans.json");
         bean.start();
         return bean;
+    }
+
+    @Value("${server.tomcat.nio}")
+    private boolean useNioConnector;
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        if (!useNioConnector) {
+            // Use BIO HTTP connector
+            tomcat.setProtocol("org.apache.coyote.http11.Http11Protocol");
+            // Disable HTTP KeepAlive
+            tomcat.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+                @Override
+                public void customize(Connector connector) {
+                    connector.setAttribute("maxKeepAliveRequests", 1);
+                }
+            });
+        }
+        return tomcat;
     }
 }
