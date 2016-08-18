acbb292622f252fafca4f45bea785c978a36ea7e
Paolo Ambrosio <git@paolo.ambrosio.name>
2016-08-18 20:51:52 +0100

    Load plugins before using them

--- README.txt
+++ README.txt
@@ -1,2 +1,13 @@
 Experiment on how to implement plugins in C++
 
+DONE
+ - Linked Boost system static in one library and dynamic in the other
+
+
+TODO
+ - Try cross-compiling with msvc (different versions?) and mingw
+   They cam be built on separate machines and run on one
+ - Try with different runtimes all at the same time (from even different compilers)
+   - http://siomsystems.com/mixing-visual-studio-versions/
+   - http://www.drdobbs.com/cpp/building-your-own-plugin-framework-part/204202899
+   - http://www.codeproject.com/Articles/20648/DynObj-C-Cross-Platform-Plugin-Objects
--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 using namespace boost;
 
 int main(int argc, char** argv) {
-  if (argc <= 2) {
+  if (argc < 2) {
     std::cout << "Usage: main [plugin ...]" << std::endl;
     exit(1);
   }
 
+  std::vector<boost::shared_ptr<PluginApi>> plugins;
   for (int i=1; i<argc; i++) {
-    auto plugin = dll::import<PluginApi>(argv[i], "plugin", dll::load_mode::append_decorations);
+    plugins.push_back(dll::import<PluginApi>(argv[i], "plugin", dll::load_mode::append_decorations));
+  }
+
+  for (auto & plugin : plugins) {
     std::cout << "Hello, " << plugin->name() << "!" << std::endl;
   }
 }
