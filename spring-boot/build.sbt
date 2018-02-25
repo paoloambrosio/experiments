2c75d00e33c6b4edd73533d24197eb62b23bd910
Paolo Ambrosio <git@paolo.ambrosio.name>
2018-02-25 09:22:27 +0000

    Adding tracing to Spring Boot... still not working

--- spring-boot/build.sbt
+++ spring-boot/build.sbt
@@ -7,7 +7,10 @@ val springBootVersion = "1.5.10.RELEASE"
 
 libraryDependencies ++= Seq(
   "org.springframework.boot" % "spring-boot-starter-parent" % springBootVersion,
-  "org.springframework.boot" % "spring-boot-starter-web" % springBootVersion)
+  "org.springframework.boot" % "spring-boot-starter-web" % springBootVersion,
+  "com.uber.jaeger" % "jaeger-core" % "0.24.0",
+  "io.opentracing.contrib" % "opentracing-spring-cloud-starter" % "0.1.6"
+)
 
 dockerBaseImage := "openjdk:8-jre-alpine"
 dockerUpdateLatest := true
--- spring-boot/src/main/java/net/paoloambrosio/Server.java
+++ spring-boot/src/main/java/net/paoloambrosio/Server.java
@@ -1,7 +1,9 @@
 package net.paoloambrosio;
 
+import io.opentracing.Tracer;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,11 @@ public class Server {
     @ResponseBody
     String home() {
         return "Hello World!";
+    }
+
+    @Bean
+    public Tracer tracer() {
+        return com.uber.jaeger.Configuration.fromEnv().getTracer();
     }
 
     public static void main(String[] args) throws Exception {
