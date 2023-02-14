76279d4193868488c0f05d554eb7e8c573ad72ab
Paolo Ambrosio <git@paolo.ambrosio.name>
2023-02-14 07:49:00 +0000

    Read and print KN5 file - Missing animated mesh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::math::{Mat4, Vec2, Vec3};
 use byteorder::{ReadBytesExt, LittleEndian};
 use core::panic;
 use std::fs::File;
@@ -16,78 +17,98 @@ fn main() {
         input.seek_relative(4).unwrap()
     }
 
-    // textures
-    let textures_len = read_i32(&mut input);
-    println!("{} textures:", textures_len);
-    for _ in 0..textures_len {
-        let type_id = read_i32(&mut input);
-        let name = read_string(&mut input);
-        let _content = read_binary(&mut input);
-        println!("- {} {}", type_id, name)
-    }
+    print_textures(&mut input);
+    print_materials(&mut input, version);
+    print_nodes(&mut input);
+}
 
-    // materials
-    let materials_len = read_i32(&mut input);
-    println!("{} materials:", materials_len);
-    for _ in 0..materials_len {
-        let name = read_string(&mut input);
-        let shader = read_string(&mut input);
-        println!("- {} {}", name, shader);
-        input.seek_relative(2).unwrap();
-        if version > 4 {
-            input.seek_relative(4).unwrap()
-        }
-        let shaders_len = read_i32(&mut input);
-        println!("  {} shaders", shaders_len);
-        for _ in 0..shaders_len {
-            let name = read_string(&mut input);
-            let value = read_f32(&mut input);
-            println!("  - {} {}", name, value);
-            input.seek_relative(36).unwrap();
-        }
-        let textures_len = read_i32(&mut input);
-        println!("  {} textures", textures_len);
-        for _ in 0..textures_len {
-            let sample_name = read_string(&mut input);
-            let sample_slot = read_i32(&mut input);
-            let texture_name = read_string(&mut input);
-            println!("  - {} {} {}", sample_name, sample_slot, texture_name);
-        }
-    }
+fn print_textures<R: Read + Seek>(input: &mut R) {
+  let textures_len = read_i32(input);
+  println!("{} textures:", textures_len);
+  for _ in 0..textures_len {
+      let type_id = read_i32(input);
+      let name = read_string(input);
+      let _content = read_binary(input);
+      println!("- {} {}", type_id, name)
+  }
+}
 
-    // nodes
-    println!("nodes:");
-    let type_id = read_i32(&mut input);
-    let name = read_string(&mut input);
-    let children_len = read_i32(&mut input);
-    input.seek_relative(1).unwrap();
-    match type_id {
-        1 => {
-            println!("  - dummy {}", name);
-            input.seek_relative(4*4*4).unwrap() // TODO
-        }
-        2 => {
-            println!("  - mesh {}", name);
-            input.seek_relative(3).unwrap();
-            let vertices_len = read_i32(&mut input);
-            println!("  {} vertices", vertices_len);
-            for _ in 0..vertices_len {
-                input.seek_relative(4*3).unwrap(); // TODO
-                input.seek_relative(4*3).unwrap(); // TODO
-                input.seek_relative(4*2).unwrap(); // TODO
-                input.seek_relative(4*3).unwrap(); // TODO
-            }
-        }
-        3 => {
-            println!("  - animated mesh {}", name);
-            unimplemented!();
-        }
-        _ => panic!("Unknown node type")
-    }
-    println!("{} children:", children_len);
-    for _ in 0..children_len {
-        unimplemented!(); // TODO this needs to be a recursive call
-    }
+fn print_materials<R: Read + Seek>(input: &mut R, version: i32) {
+  let materials_len = read_i32(input);
+  println!("{} materials:", materials_len);
+  for _ in 0..materials_len {
+      let name = read_string(input);
+      let shader = read_string(input);
+      println!("- {} {}", name, shader);
+      input.seek(SeekFrom::Current(2)).unwrap();
+      if version > 4 {
+        input.seek(SeekFrom::Current(4)).unwrap();
+      }
+      let shaders_len = read_i32(input);
+      println!("  {} shaders", shaders_len);
+      for _ in 0..shaders_len {
+          let name = read_string(input);
+          let value = read_f32(input);
+          println!("  - {} {}", name, value);
+          input.seek(SeekFrom::Current(36)).unwrap();
+      }
+      let textures_len = read_i32(input);
+      println!("  {} textures", textures_len);
+      for _ in 0..textures_len {
+          let sample_name = read_string(input);
+          let sample_slot = read_i32(input);
+          let texture_name = read_string(input);
+          println!("  - {} {} {}", sample_name, sample_slot, texture_name);
+      }
+  }
+}
+
+fn print_nodes<R: Read + Seek>(input: &mut R) {
+  println!("nodes:");
+  print_node(input);
+}
+
+fn print_node<R: Read + Seek>(input: &mut R) {
+  let type_id = read_i32(input);
+  let name = read_string(input);
+  let children_len = read_i32(input);
+  input.seek(SeekFrom::Current(1)).unwrap();
+  match type_id {
+      1 => {
+          println!("  - dummy {}", name);
+          let _transformation = read_mat4(input);
+      }
+      2 => {
+          println!("  - mesh {}", name);
+          input.seek(SeekFrom::Current(3)).unwrap();
+          let vertices_len = read_i32(input);
+          println!("  {} vertices", vertices_len);
+          for _ in 0..vertices_len {
+            let _position = read_vec3(input);
+            let _normal = read_vec3(input);
+            let _texture = read_vec2(input);
+            let _tangents = read_vec3(input);
+          }
+          let indices_len = read_i32(input);
+          println!("  {} indices", indices_len);
+          for _ in 0..indices_len {
+            let index = read_u16(input);
+            println!("  - {}", index);
+          }
+          let material_id = read_i32(input);
+          println!("  material {}", material_id);
+          input.seek(SeekFrom::Current(29)).unwrap();
+      }
+      3 => {
+          println!("  - animated mesh {}", name);
+          unimplemented!();
+      }
+      _ => panic!("Unknown node type")
+  }
+  println!("{} children:", children_len);
+  for _ in 0..children_len {
+    print_node(input);
+  }
 }
 
 fn verify_bytes<R: Read>(input: &mut R, expected: &[u8]) {
@@ -100,6 +121,10 @@ fn verify_bytes<R: Read>(input: &mut R, expected: &[u8]) {
 
 fn read_i32<R: Read>(input: &mut R) -> i32 {
     input.read_i32::<LittleEndian>().unwrap()
+}
+
+fn read_u16<R: Read>(input: &mut R) -> u16 {
+  input.read_u16::<LittleEndian>().unwrap()
 }
 
 fn read_f32<R: Read>(input: &mut R) -> f32 {
@@ -115,6 +140,25 @@ fn read_string<R: Read + Seek>(input: &mut R) -> String {
 
 fn read_binary<R: Read + Seek>(input: &mut R) -> Vec<u8> {
     let size = read_i32(input);
-    input.seek(SeekFrom::Current(size.into())).unwrap(); // TODO
-    Vec::new()
+    let mut buf = vec![0; size as usize];
+    input.read_exact(&mut buf).unwrap();
+    buf
+}
+
+fn read_mat4<R: Read>(input: &mut R) -> Mat4 {
+  let mat = [
+    [read_f32(input), read_f32(input), read_f32(input), read_f32(input)],
+    [read_f32(input), read_f32(input), read_f32(input), read_f32(input)],
+    [read_f32(input), read_f32(input), read_f32(input), read_f32(input)],
+    [read_f32(input), read_f32(input), read_f32(input), read_f32(input)],
+  ];
+  Mat4::from_cols_array_2d(&mat)
+}
+
+fn read_vec2<R: Read>(input: &mut R) -> Vec2 {
+  Vec2::new(read_f32(input), read_f32(input))
+}
+
+fn read_vec3<R: Read>(input: &mut R) -> Vec3 {
+  Vec3::new(read_f32(input), read_f32(input), read_f32(input))
 }
