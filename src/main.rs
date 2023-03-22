ddfee6de032e0852cd3cf69a3818e5dd3831d2e6
Paolo Ambrosio <git@paolo.ambrosio.name>
2023-03-22 06:55:22 +0000

    Bevy app skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use bevy::prelude::*;
+
 mod scene;
 mod kn5_loader;
 mod scene_printer;
 
 fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(print_scene)
+        .run();
+
+}
+
+fn print_scene() {
     let scene = kn5_loader::load_kn5_file("abarth500_D.kn5").unwrap();
     scene_printer::print_scene(scene);
 }
